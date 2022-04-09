1ab84a87c7b8be921744962210a21018114ea232
David <david.jergus@student.tuke.sk>
2022-04-09 09:41:02 +0200

    REST

--- src/main/java/sk/tuke/gamestudio/SpringClient.java
+++ src/main/java/sk/tuke/gamestudio/SpringClient.java
@@ -1,11 +1,14 @@
 package sk.tuke.gamestudio;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import sk.tuke.gamestudio.entity.Rating;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.client.RestTemplate;
 import sk.tuke.gamestudio.game.pegsolitaire.consoleui.ConsoleUI;
 import sk.tuke.gamestudio.game.pegsolitaire.core.GameField;
 import sk.tuke.gamestudio.service.*;
@@ -13,6 +16,8 @@ import sk.tuke.gamestudio.service.*;
 
 @SpringBootApplication
 @Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,
+        pattern = "sk.tuke.gamestudio.server.*"))
 public class SpringClient {
 
     private static final String[][] standardtest = {
@@ -25,7 +30,8 @@ public class SpringClient {
             {"null", "null", "empty", "empty", "empty", "null","null"}};
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringClient.class);
+
+        new SpringApplicationBuilder(SpringClient.class).web(WebApplicationType.NONE).run(args);
     }
 
     @Bean
@@ -39,23 +45,28 @@ public class SpringClient {
     }
 
     @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
     public GameField field(){
         return new GameField(standardtest);
     }
 
     @Bean
     public ScoreService scoreService(){
-        return new ScoreServiceJPA();
+        return new ScoreServiceRestClient();
     }
 
     @Bean
     public CommentService commentService(){
-        return new CommentServiceJPA();
+        return new CommentServiceRestClient();
     }
 
     @Bean
     public RatingService ratingService(){
-        return new RatingServiceJPA();
+        return new RatingServiceRestClient();
     }
 }
 
---
+++ src/main/java/sk/tuke/gamestudio/server/GameStudioServer.java
@@ -0,0 +1,41 @@
+package sk.tuke.gamestudio.server;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sk.tuke.gamestudio.game.pegsolitaire.consoleui.ConsoleUI;
+import sk.tuke.gamestudio.game.pegsolitaire.core.GameField;
+import sk.tuke.gamestudio.service.*;
+
+
+@SpringBootApplication
+@Configuration
+@EntityScan("sk.tuke.gamestudio.entity")
+public class GameStudioServer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GameStudioServer.class);
+    }
+
+    @Bean
+    public ScoreService scoreService(){
+        return new ScoreServiceJPA();
+    }
+
+    @Bean
+    public CommentService commentService(){
+        return new CommentServiceJPA();
+    }
+
+    @Bean
+    public RatingService ratingService(){
+        return new RatingServiceJPA();
+    }
+}
+
